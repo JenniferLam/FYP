c57173cc3ffdd4b1532fbf84e370311a90efcd4d
JenniferLam <tcpsawingting@gmail.com>
2018-02-03 04:00:37 +0800

    exported sample data from db

---
+++ WebAppDev/FYP_webapp/WebContent/prediction/dataPred.js
@@ -0,0 +1,30 @@
+fileUpload = function(){
+	
+	
+	// Display response message
+	// 1. Loading... Please wait
+	// 2. The import is successful
+	// 3. Wrong column name
+	// 4. Wrong date format
+		
+}
+
+displayUserSelection = function(){
+	// If fileUpload() returns true, then ask user to choose the time range and species
+	
+	
+}
+
+dataPrediction = function(){
+	
+	// Display response message
+	// 1. Loading... Please wait
+
+}
+
+printPredictionResult = function(){
+	
+}
+
+
+
